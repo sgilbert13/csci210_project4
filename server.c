b8b0b2f2116578f6c3871bd698c6a5928581af64
Sean Gilbert <seanlgilbert@icloud.com>
2024-12-04 16:57:53 -0700

    your commit message here

--- server.c
+++ server.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,51 +6,51 @@
 #include <signal.h>
 
 struct message {
-	char source[50];
-	char target[50]; 
-	char msg[200]; // message body
+    char source[50];
+    char target[50]; 
+    char msg[200]; // message body
 };
 
 void terminate(int sig) {
-	printf("Exiting....\n");
-	fflush(stdout);
-	exit(0);
+    printf("Exiting....\n");
+    fflush(stdout);
+    exit(0);
 }
 
 int main() {
-	int server;
-	int target;
-	int dummyfd;
-	struct message req;
-	signal(SIGPIPE,SIG_IGN);
-	signal(SIGINT,terminate);
-	server = open("serverFIFO",O_RDONLY);
-	dummyfd = open("serverFIFO",O_WRONLY);
+    int server;
+    int target;
+    int dummyfd;
+    struct message req;
+    signal(SIGPIPE, SIG_IGN);
+    signal(SIGINT, terminate);
+    server = open("serverFIFO", O_RDONLY);
+    dummyfd = open("serverFIFO", O_WRONLY);
 
-	while (1) {
-		// TODO:
-		// read requests from serverFIFO
+    while (1) {
+        // TODO:
+        // read requests from serverFIFO
+        ssize_t bytesReceived = read(server, &req, sizeof(req));
+        if (bytesReceived > 0) {
+            printf("Received a request from %s to send the message %s to %s.\n", req.source, req.msg, req.target);
 
+            // TODO:
+            // open target FIFO and write the whole message struct to the target FIFO
+            // close target FIFO after writing the message
+            target = open(req.target, O_WRONLY);
+            if (target == -1) {
+                perror("Failed to open target FIFO");
+                continue;
+            }
+            ssize_t bytesWritten = write(target, &req, sizeof(req));
+            if (bytesWritten == -1) {
+                perror("Failed to write to target FIFO");
+            }
+            close(target);
+        }
+    }
 
-
-
-
-
-		printf("Received a request from %s to send the message %s to %s.\n",req.source,req.msg,req.target);
-
-		// TODO:
-		// open target FIFO and write the whole message struct to the target FIFO
-		// close target FIFO after writing the message
-
-
-
-
-
-
-
-	}
-	close(server);
-	close(dummyfd);
-	return 0;
+    close(server);
+    close(dummyfd);
+    return 0;
 }
-
